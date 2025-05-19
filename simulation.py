229b6dca7b9339475251166dded392489287f6a1
Vithor99 <vito.reichlin@gmail.com>
2025-05-19 14:20:29 +0200

    commit simulation

--- simulation.py
+++ simulation.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 import gymnasium as gym
 from gymnasium import spaces
 import warnings
-from steady import steady
+
 
 class Model(gym.Env):
 
-    def __init__(self, k=0, var_k=0, gamma=0, psi=0, delta=0, rhoa=0, alpha=0, T=0, noise=0, u_ss = 0, version=None):
+    def __init__(self, k=0, var_k=0, gamma=0, psi=0, delta=0, rhoa=0, alpha=0, T=0, noise=0, version=None):
         super().__init__()
 
         self.observation_space = spaces.Box(
@@ -28,7 +28,6 @@ class Model(gym.Env):
         self.k0 = k
         self.var_k = var_k
         self.noise = noise
-        self.u_ss = u_ss
         self.version = version
 
     def reset(self, seed=None, options=None):
@@ -36,7 +35,7 @@ class Model(gym.Env):
         self.time = 0
         self.state[0] = 1
 
-        if options == "steady":
+        if options=="steady":
             self.state[1] = self.k0
         else:
             self.state[1] = np.random.uniform(low=self.k0*(1-self.var_k), high=self.k0*(1+self.var_k)) 
@@ -47,25 +46,24 @@ class Model(gym.Env):
 
     def step(self, action):
 
+        #rescale magnitueds coming from NN
         z = self.state[0]
         k = self.state[1]
         c = action[0]
-        #c_ratio = action[0]                                                                        
         n = action[1]
-        
+
         #compute Penalty / reward
-        y = z * (k**self.alpha) * (n**(1-self.alpha))
+        y = z* (k**self.alpha) * (n**(1-self.alpha))
         y = np.nan_to_num(y, nan=0.0)
-        #c = c_ratio*y                                                                               
 
         if (1-n) < 0 or c < 0 or n < 0 or y-c < 0:
             U = self.gamma*np.log(c)+self.psi*np.log(1-n)
-            k1 = (1-self.delta)*k + y - c                    #updates Capital level
+            k1 = (1-self.delta)*k + y - c                      #updates Capital level
             values = c/y                                       #debugging
             warnings.warn(f"Bounds are not working: {values}") #debugging
         else:
             U = self.gamma*np.log(c) + self.psi*np.log(1-n)
-            k1 = (1-self.delta)*k + y - c  # updates Capital level 
+            k1 = (1-self.delta)*k + y - c  # updates Capital level
 
         if self.version =="deterministic":
             z1 = (1-self.rhoa) + self.rhoa*z  # updates tech.lvl
@@ -82,9 +80,6 @@ class Model(gym.Env):
             done = True
 
         return new_state, U, done, False, {'y': y, 'c': c}
-
-
-
 
         
     
